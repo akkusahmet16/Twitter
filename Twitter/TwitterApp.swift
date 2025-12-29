144922a041bff30b6195e83ffd38247c8a7f1039
a.ak16 <a.ak16@hotmail.com>
2025-12-29 11:03:38 +0300

    Initial Commit

---
+++ Twitter/TwitterApp.swift
@@ -0,0 +1,17 @@
+//
+//  TwitterApp.swift
+//  Twitter
+//
+//  Created by Akkuş on 29.12.2025.
+//
+
+import SwiftUI
+
+@main
+struct TwitterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
